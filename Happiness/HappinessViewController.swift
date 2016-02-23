b78d2e5b74eaad5d195f2f06de61240f178ac623
Anechka <anechka@anechka-mac.local>
2016-02-24 01:08:03 +0300

    initial commit happiness

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Anechka on 24.02.16.
+//  Copyright © 2016 Anechka. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+    
+}
