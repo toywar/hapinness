accbccf9f82a4fb734614278eb4547e905f74a81
Anechka <anechka@anechka-mac.local>
2016-02-24 23:59:07 +0300

    add protocol, happiness at 0 to 100 (sad -> ecstatic)

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+protocol FaceViewDataSource: class {
+    func smilinessForFaceView(sender: FaceView) -> Double?
+}
+
+@IBDesignable
+
 class FaceView: UIView {
     
+    @IBInspectable
     var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    @IBInspectable
     var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    @IBInspectable
     var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
     
     var faceCenter: CGPoint {
@@ -21,6 +30,8 @@ class FaceView: UIView {
     var faceRadius: CGFloat {
         return min(bounds.size.width, bounds.size.height) / 2 * scale
     }
+    
+    weak var dataSource: FaceViewDataSource?
     
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
@@ -79,7 +90,7 @@ class FaceView: UIView {
         bazierPathForEye(.Left).stroke()
         bazierPathForEye(.Right).stroke()
         
-        let smiliness = -0.5
+        let smiliness = dataSource?.smilinessForFaceView(self) ?? 0.0
         let smilePath = bazierPathForSmile(smiliness)
         smilePath.stroke()
         
--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class HappinessViewController: UIViewController {
+class HappinessViewController: UIViewController, FaceViewDataSource {
     
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+        }
+    }
     
+    var happiness: Int = 10 { //0 - very sad, 100 - ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness - 50) / 50
+    }
 }
