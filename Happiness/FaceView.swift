f61c10a27813e1ec0b2cd10b22cd347aed1cbd44
Anechka <anechka@anechka-mac.local>
2016-02-25 18:01:13 +0300

    add gestures: pinch and pan

--- Happiness/FaceView.swift
+++ Happiness/FaceView.swift
@@ -33,6 +33,13 @@ class FaceView: UIView {
     
     weak var dataSource: FaceViewDataSource?
     
+    func scale(gesture: UIPinchGestureRecognizer) {
+        if gesture.state == .Changed {
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
+    }
+    
     private struct Scaling {
         static let FaceRadiusToEyeRadiusRatio: CGFloat = 10
         static let FaceRadiusToEyeOffsetRatio: CGFloat = 3
--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -10,19 +10,42 @@ import UIKit
 
 class HappinessViewController: UIViewController, FaceViewDataSource {
     
-    @IBOutlet weak var faceView: FaceView! {
-        didSet {
-            faceView.dataSource = self
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
         }
     }
     
-    var happiness: Int = 10 { //0 - very sad, 100 - ecstatic
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+            faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "changeHappiness:"))
+        }
+    }
+    
+    
+    var happiness: Int = 25 { //0 - very sad, 100 - ecstatic
         didSet {
             happiness = min(max(happiness, 0), 100)
             print("happiness = \(happiness)")
             updateUI()
         }
     }
+
     
     private func updateUI() {
         faceView.setNeedsDisplay()
